0e93880fe02ce0ca18af7ebb433bca0e1e9ff9ac
Sagar Karmoker <sagarkarmoker2001@gmail.com>
2022-02-09 22:06:25 +0600

    First Class Commit

---
+++ Pointer/pointer.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x = 1, y = 2;
+    cout << x << endl;
+    cout << &x << endl; // address of x
+    // pointer 
+    int *p, *q; 
+    //cout << p << endl; // gerbase value
+
+    p = &x; // Address assingment operator
+    cout << p << endl; // value assigned operator
+    cout << *p << endl; // value of x
+
+    q = &y;
+    cout << *q << endl; // now value is 2
+
+    // overwritten the value of q
+    q = p; // address of assigned operator
+
+    cout << q << endl; // address of X
+    cout << *q << endl; // value of x
+
+    return 0;
+}
